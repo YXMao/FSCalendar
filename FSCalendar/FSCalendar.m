bdbf6f2a2ae7b5d956d21ae0e5843a2c3d0b995d
maoyuxiang <295911401@qq.com>
2018-08-10 17:10:32 +0800

    Removing animation for [selectDate: scrollToDate: atMonthPosition:] method

--- FSCalendar/FSCalendar.m
+++ FSCalendar/FSCalendar.m
@@ -1203,7 +1203,7 @@ typedef NS_ENUM(NSUInteger, FSCalendarOrientation) {
         if (!shouldSelect) {
             return;
         }
-        [self scrollToPageForDate:targetDate animated:YES];
+        [self scrollToPageForDate:targetDate animated:NO];
     }
 }
 
